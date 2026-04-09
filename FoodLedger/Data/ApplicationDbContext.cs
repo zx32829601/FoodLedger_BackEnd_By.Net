c1044ffc156085d0629182061e1ee539c55e2ef5
Nick <zx32829601@gmail.com>
2026-04-09 15:37:00 +0800

    Issue:建立DailyRecord Model - 建立DailyRecord Model - 串接Swagger API測試

--- FoodLedger/Data/ApplicationDbContext.cs
+++ FoodLedger/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FoodLedger.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : DbContext
 {
@@ -8,9 +9,40 @@ public class ApplicationDbContext : DbContext
     }
 
     // 定義設定的Table Class
-
+    public DbSet<DailyRecord> DailyRecords { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<DailyRecord>(entity =>
+        {
+            // 讓 created_at 在資料庫端自動生成 now()
+            entity.Property(e => e.CreatedAt)
+                  .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            entity.Property(e => e.ModifiedAt)
+                  .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            entity.Property(e => e.RecordId)
+                  .HasDefaultValueSql("gen_random_uuid()");
+        });
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is BaseEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        foreach (var entityEntry in entries)
+        {
+            ((BaseEntity)entityEntry.Entity).ModifiedAt = DateTimeOffset.UtcNow;
+
+            if (entityEntry.State == EntityState.Added)
+            {
+                ((BaseEntity)entityEntry.Entity).CreatedAt = DateTimeOffset.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
--- FoodLedger/Program.cs
+++ FoodLedger/Program.cs
@@ -12,7 +12,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer(); // 讓 Swagger 找到你的 API
+builder.Services.AddSwaggerGen();           // 生成 Swagger 規格
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
@@ -21,6 +23,8 @@ app.MapDefaultEndpoints();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();   // 啟用 Swagger 中介軟體
+    app.UseSwaggerUI(); // 啟用 Swagger UI 網頁介面
 }
 
 app.UseHttpsRedirection();
